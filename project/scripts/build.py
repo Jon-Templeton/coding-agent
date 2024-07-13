c574240f98ced6eccdf6a8e79b4d4065200089d2
Jon Templeton <jonathan26t@gmail.com>
2024-07-13 17:54:53 -0400

    Tweak build prompt

--- project/scripts/build.py
+++ project/scripts/build.py
@@ -7,8 +7,10 @@ from llm_thread import LlmThread
 from llm_utils import get_directory_tree, read_file, modify_file, execute_terminal_command
 
 desktop_path = "/Users/jont/Desktop/blackjack/"
+# TODO: New logic for project path. Maybe use a config file to set the project path and api keys.
+# TODO: Consider removing terminal option from prompt after stage 1 or 2.
 
-def build_project(client: anthropic.Anthropic, logger: logging.Logger):
+def build_project(client: anthropic.Anthropic, logger: logging.Logger, num_dev_steps: int) -> None:
     # Load Development Plan
     with open(os.path.join(desktop_path, "agent_logs/development_plan.txt"), "r") as file:
         development_plan = file.read()
@@ -20,63 +22,58 @@ def build_project(client: anthropic.Anthropic, logger: logging.Logger):
     The task section is a list of tasks with three options: 'read', 'write' and 'terminal'. 
     To read files from the project folder, [task][type] should be 'read', [task][file_path] should be path of file to read.
     If the response has read tasks, no write or terminal task should be included. After the final read task, the final task should be [task][type] = 'Incomplete'.
-    To write to a file, [task][type] should be 'file', [task][filename] should be name of file to modify, [task][summary] is 1 sentence summary, [task][content] is the file contents. Newline characters should be escaped with '\\n'.
+    To write to a file, [task][type] should be 'write', [task][file_path] should be path of file to write, [task][summary] is 1 sentence summary, [task][content] is the file contents. Newline characters should be escaped with '\\n'.
     To install libraries and packages, [task][type] should be 'terminal', [task][command] should be the mac terminal command to install the package, [task][command_description] should be a description of the command. 
     
-    A task should include the entire file contents.
+    A write task should include the entire file contents. If the file is lengthy, only include one write task per response.
     If the stage is not complete in one response, the final task should be [task][type] = 'Incomplete'.
     """
-    # TODO: Add ability to request file contents
 
-    # Loop through each task in the development plan, manually set for now
-    for i in range(5,12):
-        logger.info(f"Building Stage {i}")
+    # Loop through each stage in the development plan
+    for i in range(1,num_dev_steps + 1):
         print(f"Building Stage {i}")
         
-        # Create prompt for each task
+        # Create stage specific prompt
         prompt_build = prompt_overview + f"Here is the outline: {development_plan}"
         prompt_build += f"current status of project folder: {get_directory_tree()}\n"
         prompt_build += f"Complete Stage {i} from project outline."
         
-        # Create LLM Thread
+        # Create LLM Thread and query the model
         build_thread = LlmThread(client, logger)
-        # Query the model
         response_json = build_thread.query_model(prompt_build)
         
+        # Process Model Response
         process_stage = True
         while process_stage:
             process_stage = False
             new_prompt = ""
-            print(f"Stage {i} Summary: {response_json['summary']}")
-            input(f"Press 'Enter' to process {len(response_json['tasks'])} tasks...")
+            print(f"Response Summary: {response_json['summary']}")
             
             # Complete each task
             for task in response_json["tasks"]:
+                
                 # Write to File
                 if task["type"] == "write":
-                    print(f"\nFile: {task['filename']}")
+                    print(f"\nWriting to File: {task['file_path']}")
                     print(f"Description: {task['summary']}")
                     
-                    # Modify file
-                    file_path = os.path.join(desktop_path, task["filename"])
-                    modify_file(file_path, task["content"])
+                    modify_file(task["file_path"], task["content"])
                     
                 # Read File
                 elif task["type"] == "read":
                     print(f"Reading file: {task['file_path']}")
-                    
                     new_prompt += read_file(task["file_path"])
+                    # TODO: If file is lengthy, passback each file in a seperate thread. Context window is not large enough for multiple read/writes of large files. Each thread would only modify their relevant file.
                     
                 # Execute Terminal Command
                 elif task["type"] == "terminal":
-                    # Execute terminal command
                     execute_terminal_command(task["command"], task["command_description"])
+                    # TODO: Passback terminal output to LLM in separate thread. Verify command was successful, troubleshoot if not.
+                    
+                # Stage not complete. Continue building...
                 elif task["type"] == "Incomplete":
                     process_stage = True
-                    
                     response_json = build_thread.query_model("Continue building stage {i}")
                 
         
-        
-        
-        input("Press 'Enter' to continue...")
+        input("Press 'Enter' to continue to next stage...")
